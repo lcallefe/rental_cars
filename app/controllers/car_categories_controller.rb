60af1ef9cb1cdd13409b08468fee2568648bee86
lcallefe <luciana_donadio@hotmail.com>
2020-08-12 06:44:47 -0300

    Admin cadastra uma categoria de carro

--- app/controllers/car_categories_controller.rb
+++ app/controllers/car_categories_controller.rb
@@ -8,4 +8,15 @@ class CarCategoriesController < ApplicationController
         @car_category = CarCategory.find(params[:id])
     end
 
+    def new
+        @car_category = CarCategory.new 
+    end
+
+    def create 
+        car_category_params = params.require(:car_category)
+                                    .permit(:name, :daily_rate, :car_insurance, :third_party_insurance)
+        @car_category = CarCategory.create (car_category_params) #faz new e ja salva
+        redirect_to car_category_path(id: @car_category.id)
+    end
+
 end
