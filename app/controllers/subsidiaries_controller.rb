a27213128b1b88bd926af7c29585c86bdc1ad4b1
lcallefe <luciana_donadio@hotmail.com>
2020-08-12 07:55:17 -0300

    Admin cadastra e edita filial

--- app/controllers/subsidiaries_controller.rb
+++ app/controllers/subsidiaries_controller.rb
@@ -8,4 +8,27 @@ class SubsidiariesController < ApplicationController
         @subsidiary = Subsidiary.find(params[:id])
     end
 
+    def new
+        @subsidiary = Subsidiary.new
+    end
+
+    def edit
+        @subsidiary = Subsidiary.find(params[:id])
+    end
+
+    def update
+        @subsidiary = Subsidiary.find(params[:id])
+        subsidiary_params = params.require(:subsidiary).permit(:name, :address, :cnpj)
+        @subsidiary = Subsidiary.update (subsidiary_params)
+        redirect_to subsidiaries_path
+    end
+
+
+    def create
+        puts params
+        subsidiary_params = params.require(:subsidiary).permit(:name, :address, :cnpj)
+        @subsidiary = Subsidiary.create (subsidiary_params)
+        redirect_to subsidiary_path(id: @subsidiary.id)
+    end
+
 end
