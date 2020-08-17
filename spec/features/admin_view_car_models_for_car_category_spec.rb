2bbfad53721f9c0b8b5351c4eb9e50cca2b21d99
lcallefe <luciana_donadio@hotmail.com>
2020-08-17 14:01:07 -0300

    WIP: Admin vê modelos de carro de uma categoria

---
+++ spec/features/admin_view_car_models_for_car_category_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+feature 'Admin view car models for a given category' do
+  scenario 'successfully' do
+    car_category = CarCategory.create!(name: 'Top', daily_rate: 200, car_insurance: 50,
+                                           third_party_insurance: 20)
+    CarModel.create!(name: 'Chevette', year: 1974, manufacturer: 'Chevrolet', 
+                     motorization:'1.6', car_category: car_category, fuel_type: 'Alcool')
+    
+
+    visit root_path
+    click_on 'Categorias'
+    click_on 'Top'
+    click_on 'Ver modelos para esta categoria'
+
+    expect(page).to have_content('Chevette')
+    expect(page).to have_content('1974')
+    expect(page).to have_content('Chevrolet')
+    expect(page).to have_content('Top')
+    expect(page).to have_content('Alcool')
+  end
+
+  xscenario 'and there is more than one car model for that car category' do
+  end
+
+  xscenario 'and car category has no car model associated with' do
+
+  end 
+
+end
