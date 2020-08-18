6566a081e109bc978b6bd791c12f923dfeaf6288
lcallefe <luciana_donadio@hotmail.com>
2020-08-18 12:13:53 -0300

    Validação de CarModel

--- app/models/car_model.rb
+++ app/models/car_model.rb
@@ -2,4 +2,5 @@ class CarModel < ApplicationRecord
   belongs_to :car_category
   validates :name, :year, :manufacturer, :motorization,
               :fuel_type, :car_category_id, presence: true
+  validates_uniqueness_of :name, :scope => :car_category_id, :message => 'Modelo de carro já existente para a categoria'
 end
--- spec/models/car_model_spec.rb
+++ spec/models/car_model_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
-RSpec.describe CarModel, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+require 'rails_helper'
+
+describe CarModel, type: :model do
+  context 'validation' do
+    it 'attributes cannot be blank' do
+      car_model = CarModel.new
+
+      car_model.valid?
+
+      expect(car_model.errors[:name]).to include('não pode ficar em branco')
+      expect(car_model.errors[:year]).to include('não pode ficar em branco')
+      expect(car_model.errors[:motorization]).to include('não pode ficar em branco')
+      expect(car_model.errors[:fuel_type]).to include('não pode ficar em branco')
+      expect(car_model.errors[:car_category_id]).to include('não pode ficar em branco')
+
+    end
+
+    it 'car model name must be uniq for car category' do
+      category = CarCategory.create!(name: 'Duplicada', daily_rate: 105.5, car_insurance: 58.5,
+                          third_party_insurance: 10.5)
+      CarModel.create!(name: 'Fiesta', year: 2002, manufacturer: 'Ford', 
+                          motorization:'1.0', car_category: category, fuel_type: 'Flex')
+      car_model = CarModel.new(name: 'Fiesta', year: 2005, manufacturer: 'Volkswagen', 
+                          motorization:'1.6', car_category: category, fuel_type: 'Gasolina')
+
+      car_model.valid?
+
+      expect(car_model.errors[:name]).to include('Modelo de carro já existente para a categoria')
+    end
+  end
 end
+
